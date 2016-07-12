421cda06effefaac854d96301cf52f36ab40d7e1
Elijah Cooke <elijahjcooke@gmail.com>
2016-07-12 08:13:55 -0400

    created get function for mouseaction in the preference file

--- modules/morphlib.preferences.js
+++ modules/morphlib.preferences.js
@@ -2,6 +2,18 @@
  * Created by Elijah Cooke on 7/12/2016.
  */
 class preferences {
-    mouseaction = {}
+    mouseActions = {}
+    constructor(prefFile) {
+        //TODO add read in from preference file
+    }
+    setMouseAction (lang, newAction){
+        if(mouseActions(lang)){
+            mouseActions(lang) 
+        }
+    }
+
+    getMouseAction (lang){
+        return mouseActions(lang);
+    }
 }
 export default preferences;
