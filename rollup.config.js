38d2efd1282af75cd7a1190435e0e02d8c394dd7
Bridget Almas <balmas@gmail.com>
2016-07-29 19:38:04 -0400

    cleanup rollup config build should always be to a different directory we want to use iife not cjs

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,15 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-    entry: 'out/morphlib.main.js',
-    format: 'cjs',
-    plugins: [ babel() ],
-    dest: 'bundle.js'
+    entry: 'modules/morphlib.main.js',
+    plugins: [ 
+      babel({exclude: 'node_modules/**'}) 
+    ],
+    moduleName: 'morphlib',
+    targets: [
+      { 
+        dest: "build/js/bundle.js",
+        format: "iife"
+      }
+    ]
 };
