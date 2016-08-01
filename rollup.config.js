62e607ee1761c47f67b810799f21d421d35de650
Elijah Cooke <elijahjcooke@gmail.com>
2016-08-01 19:55:18 -0400

    Changed rollup bundle output name to something more meaningful

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ export default {
     moduleName: 'morphlib',
     targets: [
       { 
-        dest: "build/js/bundle.js",
+        dest: "build/js/alpheios-morphlib.js",
         format: "iife"
       }
     ]
